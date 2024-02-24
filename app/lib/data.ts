5387158fc0ec4df887410b2a932d7dd2b128a6cc
Hscioscia <hernan.scioscia@gmail.com>
2024-02-24 14:27:25 -0300

    Dynamic dashboard,Loading Skeleton, Fix Issue Async components, Invoices Skeleton

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,15 +9,13 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
-  // Add noStore() here to prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  
+  noStore();
 
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
      console.log('Fetching revenue data...');
      await new Promise((resolve) => setTimeout(resolve, 3000));
 
@@ -33,6 +31,9 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+
+  noStore();
+
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -53,6 +54,9 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+
+  noStore();
+
   try {
 
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
@@ -90,6 +94,8 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
+
+  noStore();
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -122,6 +128,9 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+
+  noStore();
+
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -143,6 +152,9 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  
+  noStore();
+
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -186,6 +198,9 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+
+  noStore();
+
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,15 +2,12 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
-import React from 'react';
+import { fetchLatestInvoices } from '@/app/lib/data';
 
-//Chequear el Async de la function!
-export default  function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+export default async function LatestInvoices() {
+  
+  const latestInvoices = await fetchLatestInvoices();
+
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
